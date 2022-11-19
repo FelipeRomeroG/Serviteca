846c34429a978496d88363be5071cfa3e9e5916e
Guillermo Romero <guillermo_romero_galvis@hotmail.com>
2022-11-19 13:42:46 -0500

    Sprint 4 Final

--- FrontendServ/src/app/modulos/vehiculos/vehiculos-routing.module.ts
+++ FrontendServ/src/app/modulos/vehiculos/vehiculos-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearComponent } from './crear/crear.component';
 import { EditarComponent } from './editar/editar.component';
+import { EliminarComponent } from './eliminar/eliminar.component';
 import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
   {path: '', component:IndexComponent},
   {path: 'crear', component: CrearComponent},
   {path: 'editar/:id', component: EditarComponent},
+  {path: 'eliminar/:id', component: EliminarComponent},
   
 ];
 
--- FrontendServ/src/app/modulos/vehiculos/vehiculos.module.ts
+++ FrontendServ/src/app/modulos/vehiculos/vehiculos.module.ts
@@ -7,12 +7,14 @@ import { CrearComponent } from './crear/crear.component';
 
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { EditarComponent } from './editar/editar.component';
+import { EliminarComponent } from './eliminar/eliminar.component';
 
 @NgModule({
   declarations: [
     IndexComponent,
     CrearComponent,
     EditarComponent,
+    EliminarComponent,
   ],
   imports: [
     CommonModule,
